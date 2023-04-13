632ac5466af3bdebf755b316618471921272ce86
Tony Terrasa <tony.terrasa@gmail.com>
2023-04-13 18:24:54 +0200

    ssg black formatting

--- ssg.py
+++ ssg.py
@@ -5,37 +5,34 @@ import sys
 
 
 # using from https://realpython.com/primer-on-jinja-templating/#include-a-navigation-menu
-SOURCE_FOLDER = 'src/'
+SOURCE_FOLDER = "src/"
 
 environment = Environment(loader=FileSystemLoader(SOURCE_FOLDER))
 
 
 def render(filename: str):
-        template = environment.get_template(filename)
-        content = template.render(date=date.today())
-        with open(filename, mode="w", encoding="utf-8") as outfile:
-                outfile.write(content)
-                print(f"... wrote {filename}")
-    
-
-if __name__ == '__main__':
+    template = environment.get_template(filename)
+    content = template.render(date=date.today())
+    with open(filename, mode="w", encoding="utf-8") as outfile:
+        outfile.write(content)
+        print(f"... wrote {filename}")
 
 
-    # arguments determine what files to render 
-    if len(sys.argv) == 1 or sys.argv[1] == '-a':
+if __name__ == "__main__":
+
+    # arguments determine what files to render
+    if len(sys.argv) == 1 or sys.argv[1] == "-a":
         # render everything if -a or nothing given
-        render_files = 'all'
+        render_files = "all"
     else:
         # otherwise render only the given files
         render_files = sys.argv[1:]
-        
 
     for root, dirs, files in walk(SOURCE_FOLDER):
         for filename in files:
             # don't render files with _ at the beginning
-            if filename[0] == '_':
+            if filename[0] == "_":
                 continue
 
-            if render_files == 'all' or filename in render_files:
+            if render_files == "all" or filename in render_files:
                 render(filename)
-
